ad8cae016067e959103bd9643b19ff38962f8390
GLaDOS-418 <arnobbhanja@live.com>
2023-04-10 03:06:55 +0530

    WIP: thread pool.

--- src/include/thread_pool.h
+++ src/include/thread_pool.h
@@ -1,47 +1,56 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+
+#include <algorithm>
+#include <atomic>
 #include <cstddef>
+#include <functional>
 #include <iterator>
 #include <thread>
 #include <vector>
-#include <functional>
-#include <algorithm>
-#include <atomic>
 
-#include "multithreaded_queue.h"
+#include "concurrent_block_queue.h"
 
-class ThreadJoiner {
-  std::vector<std::thread>& joinable_threads;
+namespace utility {
 
-  public:
-  ThreadJoiner(std::vector<std::thread>& threads) : joinable_threads{threads}
-  {}
-  
-  ~ThreadJoiner() {
-    std::for_each(std::begin(joinable_threads),std::end(joinable_threads),[](auto& _thread) {
+  class ThreadJoiner {
+    std::vector<std::thread> &joinable_threads;
+
+    public:
+    ThreadJoiner(std::vector<std::thread> &threads) : joinable_threads{threads} {}
+
+    ~ThreadJoiner() {
+      std::for_each(std::begin(joinable_threads), std::end(joinable_threads),
+          [](auto &_thread) {
           if (_thread.joinable()) {
-             _thread.join() ;
+          _thread.join();
           }
-        });
-  }
-};
-
-class thread_pool {
-  using WorkerPool = std::vector<std::thread>;
-
-  ThreadsafeQ<std::function<void ()>> tasks;
-  WorkerPool workers;
-  ThreadJoiner joiner; 
-  std::atomic_bool  done;
-
-  void worker_method() {
-    while(!done) {
-      auto work = tasks.wait_and_pop();
-      work();
+          });
     }
-  }
-  public:
-  thread_pool(const size_t total_threads = std::thread::hardware_concurrency()) : joiner(workers), done(false){
-    WorkerPool(total_threads, std::thread(&thread_pool::worker_method));
-  }
+  };
 
+  class thread_pool {
+    using WorkerPool = std::vector<std::thread>;
 
-};
+    ds::ConcurrentBlockQueue<std::function<void()>> tasks;
+    WorkerPool workers;
+    ThreadJoiner joiner;
+    std::atomic_bool done;
+
+    void worker_method() {
+      while (!done) {
+        auto work = tasks.wait_and_pop();
+        work();
+      }
+    }
+
+    public:
+    thread_pool(const size_t total_threads = std::thread::hardware_concurrency())
+      : joiner(workers), done(false)
+    {
+        //WorkerPool(total_threads, std::thread(&utility::thread_pool::worker_method,this)).swap(workers);
+    }
+  };
+}; // namespace utility
+
+#endif // THREAD_POOL_H
