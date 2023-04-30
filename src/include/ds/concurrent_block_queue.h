cc1e2de15aa19ffa893bbe696a06cc7bed3817e8
GLaDOS-418 <arnobbhanja@live.com>
2023-04-30 20:35:48 +0530

    add clear-only mode, fix move on push

--- src/include/ds/concurrent_block_queue.h
+++ src/include/ds/concurrent_block_queue.h
@@ -7,8 +7,11 @@
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <stdexcept>
+#include <system_error>
 #include <utility>
 #include <vector>
+#include <functional>
 
 namespace ds {
   template <typename T, size_t BLOCK_SIZE = 512> 
@@ -95,8 +98,8 @@ namespace ds {
         }
       }
 
-      inline void add_data( T val) {
-        tail_block->data.emplace_back(val);
+      inline void add_data( T&& val) {
+        tail_block->data.emplace_back(std::move(val));
         update_tail();
       }
     };
@@ -106,8 +109,16 @@ namespace ds {
     /////////////////////////////////////////////
     Head m_head;
     Tail m_tail;
+
     std::atomic<size_t> m_size{0};
-    std::condition_variable push_pop_sync;
+    std::condition_variable m_queue_signal;
+
+    /*
+     * - controlled by client
+     *   true = no push, no wait, only pop
+     *   false = push and pop available
+     */
+    std::atomic<bool> m_clear_mode_enabled{false}; 
 
     /////////////////////////////////////////////
     ///  PUBLIC API
@@ -129,11 +140,18 @@ namespace ds {
     ConcurrentBlockQueue& operator= ( ConcurrentBlockQueue&& ) = default;
     ~ConcurrentBlockQueue( ) = default;
 
-    void push( T&& val ){
+    /*  
+     *  retval: 0= SUCCESS, 1= FAILED
+     */
+    size_t push( T&& val ){
+      if(m_clear_mode_enabled) return 1;
+
       std::lock_guard<std::mutex> guard(m_tail.lock);
       m_tail.add_data(std::move(val));
       ++m_size;
-      push_pop_sync.notify_one();
+      m_queue_signal.notify_one();
+
+      return 0; // success
     }
 
     std::optional<T> try_pop( ) {
@@ -147,14 +165,22 @@ namespace ds {
       return { };
     }
 
-    T wait_and_pop( ) {
+    std::optional<T> wait_and_pop( ) {
       std::unique_lock<std::mutex> guard(m_head.lock);
-      push_pop_sync.wait(guard, [this]( ){ return not empty( ); } );
+      m_queue_signal.wait(guard, [this]( ){ return m_clear_mode_enabled || not empty( ); } );
+
+      if(m_clear_mode_enabled && empty( ))
+        return { };
 
       auto data{m_head.pop_data( )};
       --m_size;
 
       return data;
+    }
+
+    void enable_clear_mode( ) {
+      m_clear_mode_enabled=true;
+      m_queue_signal.notify_all( );
     }
 
     bool empty( ){
