710c4a3f138efb1a8d01df9bdf7451e067c54427
GLaDOS-418 <arnobbhanja@live.com>
2023-05-20 03:00:40 +0530

    a friendly spin lock.

--- include/util/spin_lock.h
+++ include/util/spin_lock.h
@@ -2,18 +2,32 @@
 #define SPIN_LOCK_H
 
 #include <atomic>
+#include <thread>
 
 class SpinLock {
-  std::atomic_flag m_lock = ATOMIC_FLAG_INIT;
+  std::atomic<bool> m_lock{false};
 
   public:
 
   void lock( ){
-    while(m_lock.test_and_set(std::memory_order_acquire));
+
+    // check if locked read only, if not locked, then try to lock
+    // there's a small window b/w the first and the second check which can lead to 2 failures
+    for( unsigned short i=0; 
+        m_lock.load(std::memory_order_relaxed) || m_lock.exchange(true,std::memory_order_acquire);
+        ++i){
+
+      // acquiring lock by this trhead depends on releasing lock on another thread
+      // the idea of this part is to let the other thread get scheduled to unlock
+      if(i & 8){ // 8 is a random no. needs benchmarking
+        i=0;
+        std::this_thread::yield(); // this could be a sleep statemen. again, benchmark!!
+      }
+    }
   }
 
   void unlock( ){
-    m_lock.clear( std::memory_order_release);
+    m_lock.store( false, std::memory_order_release);
   }
 
 };
